7b40b17b70fc1278ba4ac6662134945594978ba7
MHM <mhmousavi77@gmail.com>
2020-02-22 22:40:59 +0330

    added modeles

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,11 +11,6 @@ def index(request):
     return HttpResponse(output)
 
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
